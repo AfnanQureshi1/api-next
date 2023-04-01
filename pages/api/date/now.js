3d021243210e5aea1740af39c3a5a492f47bfe7c
Afnan Qureshi <Afnan.qureshi.x1@gmail.com>
2023-04-01 23:59:29 +0500

    Cache-Control api

--- pages/api/date/now.js
+++ pages/api/date/now.js
@@ -2,5 +2,6 @@ export default function now(req, res) {
     const date = new Date();
     const format = date.toGMTString();
 
+    res.setHeader("Cache-Control", "s-maxage=10,stale-while-revalidate"); //cannot test locally- have to deploy on vercel to test
     res.json({now: format});
 }
